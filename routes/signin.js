e6f2c35da8644c486119939c8488ee2a38e93c93
go-tiger <inter_tiger_@naver.com>
2023-02-02 22:15:34 +0900

    fix: body 데이터 및 jwt 코드 수정

--- routes/signin.js
+++ routes/signin.js
@@ -10,18 +10,20 @@ const bcrypt = require('bcrypt');
 const { JWT_SECRET_KET } = process.env;
 
 router.post('/login', async (req, res) => {
-  const { userName, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const User = await user.findOne({ userName, raw: true });
-
+    const User = await user.findOne({ email });
     const inPasswordCorrect = await bcrypt.compare(password, User.password);
 
     if (!User || !inPasswordCorrect) {
-      return res.status(400).json({ message: '테스트' });
+      return res
+        .status(400)
+        .json({ message: '이메일이나 비밀번호가 틀렸습니다!.' });
     }
+    const userId = await User.dataValues.id;
 
-    res.json({ token: jwt.sign({ userName }, JWT_SECRET_KET) });
+    res.json({ token: jwt.sign({ userId }, JWT_SECRET_KET) });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
