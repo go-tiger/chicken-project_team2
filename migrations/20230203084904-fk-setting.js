5f6f14e71fba50986e0a6bd524041f363accdfae
go-tiger <inter_tiger_@naver.com>
2023-02-03 18:37:23 +0900

    feat: DB sequelize setting - chicken 테이블 오타수정 - mycart 테이블 추가 - foreign key 설정

---
+++ migrations/20230203084904-fk-setting.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  /*
+    myCart 테이블에서 user 테이블 id컬럼 fk
+  */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('myCart', 'userId', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.addConstraint('myCart', {
+      fields: ['userId'],
+      type: 'foreign key',
+      name: 'myCart_userId_fk',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade',
+    });
+
+    /*
+      myCart 테이블에서 chickenMenu 테이블 id컬럼 fk
+    */
+    await queryInterface.addColumn('myCart', 'menuId', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.addConstraint('myCart', {
+      fields: ['menuId'],
+      type: 'foreign key',
+      name: 'myCart_menuId_fk',
+      references: {
+        table: 'chickenMenu',
+        field: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade',
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
