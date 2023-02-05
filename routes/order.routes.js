45c108473a540ccfada93083e5ae2a03807ecfa6
go-tiger <inter_tiger_@naver.com>
2023-02-05 21:58:24 +0900

    feat: 오더 등록 api(미완성)

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -5,7 +5,7 @@ const { order } = require('../models');
 
 /* 전체 오더 목록 */
 router.get('/', async (req, res) => {
-  // console.log(req.headers);
+  console.log(req.headers);
   const { cookie } = req.headers;
 
   if (!cookie) {
@@ -23,6 +23,35 @@ router.get('/', async (req, res) => {
     return res.status(200).json({ message: '전체 오더목록을 불러왔습니다.' });
   } catch (err) {
     res.status(400).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
+  }
+});
+
+/* 오더 주문 하기 */
+router.post('/:userId', async (req, res) => {
+  // const { cookie } = req.headers;
+
+  // if (!cookie) {
+  //   return res.status(401).json({ message: '로그인 후 이용가능합니다.' });
+  // }
+
+  try {
+    const { userId } = req.params;
+    const { orderList, address, memo, totalPrice } = req.body;
+    // console.log('userid: ', userId);
+    // console.log('body: ', req.body);
+
+    const addOrder = await order.create({
+      orderList,
+      address,
+      memo,
+      totalPrice,
+      userId,
+    });
+    console.log(addOrder);
+
+    res.status(200).json({ message: '200' });
+  } catch {
+    res.status(400).json({ message: '400' });
   }
 });
 
