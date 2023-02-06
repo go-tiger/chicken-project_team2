497a9c3d0992672d3534b7266d128557772e8c13
go-tiger <inter_tiger_@naver.com>
2023-02-06 17:23:15 +0900

    refactor: 불필요 코드 삭제

--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -17,42 +17,24 @@ router.get('/admin/menu', authMWRouter, async (req, res) => {
   } catch (error) {}
 });
 
-router.post('/admin/menu', async (req, res) => {
-  try {
-    const { menuName, menuPrice, menuPhoto } = req.body;
-    console.log(req.body);
-    const menuAdd = await chickenMenu.create({
-      menuName,
-      menuPrice,
-      menuPhoto,
-    });
-
-    res.status(201).json({ message: '메뉴 등록이 완료되었습니다.' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-/* 메뉴 등록 API */
+/* 메뉴 등록 API 시작*/
 /* Methud: POST | URL: /api/menu */
 router.post('/', upload.single('file'), async (req, res) => {
   try {
     const { menuName, menuPrice } = req.body;
     const imgPath = req.file.path;
-    const img = imgPath.split('\\')[2];
-    const menuPhoto = img;
-    console.log(menuPhoto);
-    const up = await chickenMenu.create({
+    const menuPhoto = imgPath.split('\\')[2];
+    await chickenMenu.create({
       menuName,
       menuPrice,
       menuPhoto,
     });
-    console.log(up);
-    res.status(201).json({ message: '업로드 완료.' });
+    res.status(201).json({ message: '메뉴 등록이 완료되었습니다.' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+/* 메뉴 등록 API 끝*/
 
 router.put('/admin/menu/:menuId', async (req, res) => {
   try {
