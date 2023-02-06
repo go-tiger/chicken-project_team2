5f144f57e5d82f0d82b6ea735e76fd9482e88c6c
bhkim0707 <bhkim0707@gmail.com>
2023-02-07 00:44:56 +0900

    feature 주문 CRUD에 필요한 GET Cart API 구현

--- routes/mycart.routes.js
+++ routes/mycart.routes.js
@@ -1,4 +1,36 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
+const authMWRouter = require('../middlewares/auth');
+
+const { myCart } = require('../models');
+const { chickenMenu } = require('../models');
+
+router.get('/', authMWRouter, async (req, res) => {
+  const userId = res.locals.user.id;
+  // const userId = req.params.userId;
+  console.log(userId);
+
+  try {
+    console.log('시도');
+    const cart = await myCart.findAll({
+      include: [
+        {
+          model: chickenMenu,
+        },
+      ],
+    });
+
+    // const cart = await myCart.findAll({
+    //   attributes: {
+    //     exclude: ['updatedAt'],
+    //   },
+    //   where: { userId: userId },
+    // });
+    // console.log(cart);
+    res.status(200).json([{ cart: cart }]);
+  } catch (error) {}
+});
 
 module.exports = router;
