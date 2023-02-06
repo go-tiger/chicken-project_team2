6f80b8b9cf2686d1d7cf1874d6cac0b797a95ec4
kihanGIM <58027260+kihanGIM@users.noreply.github.com>
2023-02-06 19:45:38 +0900

    feat/myCart router

--- routes/mycart.routes.js
+++ routes/mycart.routes.js
@@ -1,4 +1,69 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
+const authMWRouter = require('../middlewares/auth');
+
+const { myCart } = require('../models');
+
+const { JWT_SECRET_KET } = process.env;
+
+router.get('/cart/menu', authMWRouter, async (req, res) => {
+  console.log(req.body);
+  const userid = res.locals.user.id;
+  conssole.log(userid);
+  try {
+    const menu = await myCart.findAll();
+    res.json([{ Menu: menu, id: userid }]);
+  } catch (error) {}
+});
+
+router.post('/cart/menu', async (req, res) => {
+  try {
+    const mid = req.params.menuId;
+    const uid = req.locals.user.id;
+    const { menuPrice, menuAmount } = req.body;
+    // console.log(req.body);
+    const menuAdd = await myCart.create({
+      menuPrice,
+      menuAmount,
+    });
+
+    res.status(201).json({ message: '장바구니에 담겼습니다.' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put('/cart/menu/:menuId', async (req, res) => {
+  try {
+    const p = req.params.menuId;
+    const { menuPrice, menuAmount } = req.body;
+    // console.log(req.body);
+    const menuModify = await myCart.update(
+      {
+        menuPrice,
+        menuAmount,
+      },
+      {
+        where: { id: p },
+      }
+    );
+    res.status(201).json({ message: '메뉴 수정이 완료되었습니다.' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete('/cart/menu/:menuId', async (req, res) => {
+  try {
+    const p = req.params.menuId;
+    console.log(p);
+    const menuDelete = await myCart.destroy({ where: { id: p } });
+    res.status(201).json({ message: '메뉴 삭제가 완료되었습니다.' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
