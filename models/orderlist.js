2ff121fb6fbf062747550506a41e988a91a23325
bhkim0707 <bhkim0707@gmail.com>
2023-02-07 18:57:44 +0900

    feat 주문 POST 구현 / 프론트 연결 완료

--- models/orderlist.js
+++ models/orderlist.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      tableName: 'orderList',
+      tableName: 'orderLists',
       modelName: 'orderList',
     }
   );
