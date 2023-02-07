7616a9665cb8ad4215c88cbe661090f317693033
go-tiger <inter_tiger_@naver.com>
2023-02-07 17:04:30 +0900

    feat: orderlist 테이블 추가

--- migrations/20230203084904-fk-setting.js
+++ migrations/20230203084904-fk-setting.js
@@ -59,6 +59,63 @@ module.exports = {
       onUpdate: 'cascade',
       onDelete: 'cascade',
     });
+
+    /*
+      orderList 테이블에서 user 테이블 id컬럼 fk
+    */
+    await queryInterface.addColumn('orderLists', 'userId', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.addConstraint('orderLists', {
+      fields: ['userId'],
+      type: 'foreign key',
+      name: 'orderList_userId_fk',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade',
+    });
+
+    /*
+      orderList 테이블에서 chickenMenu 테이블 id컬럼 fk
+    */
+    await queryInterface.addColumn('orderLists', 'menuId', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.addConstraint('orderLists', {
+      fields: ['menuId'],
+      type: 'foreign key',
+      name: 'orderList_menuId_fk',
+      references: {
+        table: 'chickenMenu',
+        field: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade',
+    });
+
+    /*
+      orderList 테이블에서 chickenMenu 테이블 id컬럼 fk
+    */
+    await queryInterface.addColumn('orderLists', 'orderId', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.addConstraint('orderLists', {
+      fields: ['orderId'],
+      type: 'foreign key',
+      name: 'orderList_orderId_fk',
+      references: {
+        table: 'orders',
+        field: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade',
+    });
   },
 
   async down(queryInterface, Sequelize) {
--- models/order.js
+++ models/order.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       models.order.belongsTo(models.user, { foreignKey: 'userId' });
-      models.orderlist.hasMany(models.orderlist, { foreignKey: 'orderId' });
+      models.orderList.hasMany(models.orderList, { foreignKey: 'orderId' });
     }
   }
   order.init(
