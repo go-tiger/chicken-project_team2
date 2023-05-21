23b511a3a448c8b896b1088cfcbb3eb86c5dbec2
wjdrl3122 <wjdrl3122@gmail.com>
2023-05-21 23:18:47 +0900

    [리펙토링] ejs 페이지 -> 싱글페이지 정리 및 유저정보 가져올수 있는 함수 생성

--- routes/ejs.routes.js
+++ routes/ejs.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* 로그인 전 메인페이지 */
 router.get('/', (req, res) => {
-  res.render('login.ejs');
+  res.render('login.ejs')
 });
 
 /* 회원가입 페이지 */
@@ -13,52 +13,52 @@ router.get('/signup', (req, res) => {
 
 /* 로그인 후 유저가 메뉴를 볼 수 있는 메인 페이지 */
 router.get('/menu', (req, res) => {
-  return res.render('mainPage.ejs');
+  return res.render('index.ejs', { components: 'mainPage' });
 });
 
 // 오너 or 관리자 - 오더 관리 메인 페이지 
 router.get('/owner/orders', (req, res) => {
-  res.render('orderManagement.ejs');
+  res.render('index.ejs', { components: 'orderManagement'});
 });
 
 /* 유저 주문하기 */
 router.get('/checkout', (req, res) => {
-  res.render('checkout.ejs');
+  res.render('index.ejs', { components: 'checkout'});
 });
 
 /* 유저 장바구니 */
 router.get('/cart', (req, res) => {
-  res.render('cart.ejs');
+  res.render('index.ejs', { components: 'cart'});
 });
 
 /* 유저 마이페이지 */
 router.get('/mypage', (req, res) => {
-  res.render('myPage.ejs');
+  res.render('index.ejs', { components: 'myPage'});
 });
 
 /* 유저 주문정보 조회 */
 router.get('/orders', (req, res) => {
-  res.render('orderHistory.ejs');
+  res.render('index.ejs', { components: 'orderHistory'});
 });
 
 /* 관리자 유저 관리 */
 router.get('/admin/users', (req, res) => {
-  return res.render('userList.ejs');
+  return res.render('index.ejs', { components: 'userList'});
 });
 
 /* 관리자 유저 정보 수정 */
 router.get('/admin/users/edit', (req, res) => {
-      return res.render('userEdit.ejs');
+      return res.render('index.ejs', { components: 'userEdit'});
 });
 
 /* 관리자 메뉴 추가 */
 router.get('/admin/menu/add', (req, res) => {
-  res.render('menuAdd.ejs');
+  res.render('index.ejs', { components: 'menuAdd'});
 });
 
 /* 관리자 메뉴 수정 */
 router.get('/admin/menu/edit', (req, res) => {
-  res.render('menuEdit.ejs');
+  res.render('index.ejs', { components: 'menuEdit'});
 });
 
 module.exports = router;
