71cbd2396f6703e416e3ad49817f6995ca2dce31
wjdrl3122 <wjdrl3122@gmail.com>
2023-05-26 23:53:41 +0900

    [리펙토링] 메뉴 장바구니 가져오기, 수정 삭제 API 완료

--- repositories/cart.repository.js
+++ repositories/cart.repository.js
@@ -1,6 +1,33 @@
-const { User, Cart, Menu, CartMenu } = require('../models');
+const { User, Cart, Menu, Cart_Menu } = require('../models');
 
 class CartRepositories {
+
+  getCartMenus = async (userId) => {
+    try {
+      return await User.findOne({
+        where: {id: userId},
+        include: {
+          model: Cart,
+          include: {
+            model: Menu,
+            attributes: ["id", "menuName", "menuPrice"],
+            through: { attributes: ["quantity"] }
+          },
+          attributes: {
+            exclude: ["createdAt", "updatedAt"]
+          },
+        },
+        attributes: {
+          exclude: ["createdAt", "updatedAt", "password", "userType"]
+        }
+      });
+      
+
+    } catch (error) {
+      throw Error(error.message)
+    }
+  }
+
   addToCart = async (userId, menuId) => {
     try {
       const [cart, created] = await Cart.findOrCreate({where : {userId}})
@@ -9,7 +36,33 @@ class CartRepositories {
     } catch (error) {
       throw Error(error.message)
     }
-    
+  }
+
+  updateCartMenuQuantity = async (cartId, menuId, quantity) => {
+    try {
+      await Cart_Menu.update({
+        quantity
+      },{
+        where: {
+          cartId,
+          menuId
+        }
+      })
+    } catch (error) {
+      throw Error(error.message)
+    }
+  }
+
+  emptyToCart = async (cartId) => {
+    try {
+      await Cart.destroy({
+        where : {
+          id : cartId
+        }
+      })
+    } catch (error) {
+      throw Error(error.message)
+    }
   }
 }
 
--- services/cart.service.js
+++ services/cart.service.js
@@ -2,6 +2,15 @@ const CartRepositories = require('../repositories/cart.repository');
 
 class CartService {
   cartRepositories = new CartRepositories();
+
+  getCartMenus = async (userId) => {
+    try {
+      return await this.cartRepositories.getCartMenus(userId); 
+    } catch (error) {
+      throw Error(error.message)
+    }
+  }
+
   addToCart = async (userId, menuId) => {
     try {
       await this.cartRepositories.addToCart(userId, menuId);
@@ -9,6 +18,23 @@ class CartService {
       throw Error(error.message)
     }
   }
+
+  updateCartMenuQuantity = async (cartId, menuId, quantity) => {
+    try {
+      await this.cartRepositories.updateCartMenuQuantity(cartId, menuId, quantity);
+    } catch (error) {
+      throw Error(error.message)
+    }
+  }
+
+  emptyToCart = async (cartId) => {
+    try {
+      await this.cartRepositories.emptyToCart(cartId);
+    } catch (error) {
+      throw Error(error.message)
+    }
+  }
+
 }
 
 module.exports = CartService;
