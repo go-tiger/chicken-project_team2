a1cdd820ccf6744d442fb1d767c7c31ca9fd8bb0
go-tiger <inter_tiger_@naver.com>
2023-05-31 10:15:16 +0900

    [리팩토링] : user라우터 주석 정리

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,52 +4,22 @@ const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const userController = new UserController();
 
-router.get('/info', userController.getUserInfo);
-router.get('/', userController.getUser);
-router.get('/list', userController.getUserList);
-router.put('/', userController.updateUser);
+const AdminController = require('../controllers/admin.controller');
+const adminController = new AdminController();
 
-// router.get('/list/:id', userController.getOneUser);
+// 전체 유저 목록
+router.get('/list', userController.getUserList);
+// 유저 정보 가져오기
+router.get('/info', userController.getUserInfo);
+// 내 정보 가져오기
+router.get('/', userController.getUser);
+// 내 정보 수정
+router.put('/', userController.updateUser);
+// 회원 삭제
+router.delete('/:userId', adminController.deleteUser);
+
 // router.patch('/:userId', authMWRouter, userController.editUser);
 // router.delete('/:userId', userController.deleteUsers);
-
-// router.get('/', authMWRouter, async (req, res) => {
-//   const userId = res.locals.user.id;
-
-//   try {
-//     const User = await user.findOne({
-//       attributes: { exclude: ['password'] },
-//       where: { id: userId },
-//     });
-//     res.status(200).json([{ user: User }]);
-//   } catch (error) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
-// router.put('/edit', authMWRouter, async (req, res) => {
-//   const userId = res.locals.user.id;
-//   try {
-//     const { password, phone, address } = await editValidation.validateAsync(
-//       req.body
-//     );
-//     const hashedPassword = await bcrypt.hash(password, 12);
-//     await user.update(
-//       {
-//         password: hashedPassword,
-//         phone: phone,
-//         address: address,
-//       },
-//       { where: { id: userId } }
-//     );
-//     res.status(202).json({ message: '회원 정보가 수정되었습니다.' });
-//   } catch (error) {
-//     if (error.isJoi) {
-//       return res.status(422).json({ message: error.details[0].message });
-//     }
-//     res.status(500).json({ message: error.message });
-//   }
-// });
 
 // router.get('/admin/users', async (req, res) => {
 //   try {
